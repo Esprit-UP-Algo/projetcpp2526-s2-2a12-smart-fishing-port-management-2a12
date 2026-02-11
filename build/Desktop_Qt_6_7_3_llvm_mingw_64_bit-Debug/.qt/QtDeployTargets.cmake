d87ab4dd4a55b258cbbcdaf37f54228a501dd926
Ayoub Charyak <Charyak.Ayoub@esprit.tn>
2026-02-11 19:13:11 +0100

    Fix: Remove build artifacts and add .gitignore

--- build/Desktop_Qt_6_7_3_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_VisionSightPoissons_FILE C:/Users/GIGABYTE/OneDrive - ESPRIT/Bureau/Suivi des capture iinterface/build/Desktop_Qt_6_7_3_llvm_mingw_64_bit-Debug/VisionSightPoissons.exe)
-set(__QT_DEPLOY_TARGET_VisionSightPoissons_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_VisionSightPoissons_RUNTIME_DLLS C:/Qt/6.7.3/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/llvm-mingw_64/bin/Qt6Sql.dll;C:/Qt/6.7.3/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/llvm-mingw_64/bin/Qt6Core.dll)
